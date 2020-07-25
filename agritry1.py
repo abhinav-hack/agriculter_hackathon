fd6f48263eda03b344d9d9a0f93b4144edb3d2db
abhihack <abhijeetbhavankar309@gmail.com>
2020-07-25 15:58:36 +0530

    data is cleaned and model is loaded
    id column is sorted by removing 'F'

--- agritry1.py
+++ agritry1.py
@@ -24,6 +24,28 @@ df.Number_Weeks_Used.fillna(0, inplace=True)
 Crop_Damage = df.pop('Crop_Damage')
 out_count = len(Crop_Damage.unique())
 
+# cleaning unique id removing F
+id = df['ID']
+id = [hold[1:] for hold in id]
+df.pop('ID')
+df.insert(0, 'ID', id)
+
+'''
+#converting data to array
+x_train = df.to_numpy()
+y_train = Crop_Damage.to_numpy()
+
 # Create a model
 
-model = XGBClassifier(booster='gbtree')
+model = XGBClassifier(booster='gbtree', objective='multi:softmax',
+    learning_rate=0.1, eval_metric="auc",
+    max_depth=5, subsample=0.9, 
+    colsample_bytree=0.8, num_class=out_count,
+    n_estimators=200, gamma=0)
+
+%time model.fit(x_train, y_train, verbose=True)
+
+val = model.predict(x_train)
+
+print(accuracy_score(y_train, val))
+'''
